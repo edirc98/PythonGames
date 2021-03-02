888187ff14a1a6b732bbe6865061142ddfed4ff0
edirc98 <edirc98@gmail.com>
2021-03-02 20:13:56 +0100

    Players movement

--- Pong/clases/playerClass.py
+++ Pong/clases/playerClass.py
@@ -2,16 +2,14 @@ import pygame
 
 class Player:
     __PlayersColor = (255,255,255)
-    def __init__(self,widht,heigh,x,y,speed,screen):
+    def __init__(self,widht,heigh,x,y,screen):
         self.__rect = pygame.rect.Rect(x,y,widht,heigh)
-        self.__speed = speed
         self.__color = self.__PlayersColor
         self.__screen = screen
 
-    #Getters & Setters
 
     #Functions
-    def movePlayer(self):
-        pass
+    def move(self,speed):
+        self.__rect.y += speed
     def draw(self):
         pygame.draw.rect(self.__screen,self.__color,self.__rect)
--- Pong/main.py
+++ Pong/main.py
@@ -13,10 +13,11 @@ screen_width,screen_heigh = 1280,720
 screen = pygame.display.set_mode((screen_width,screen_heigh))
 pygame.display.set_caption("Pong")
 screen.fill(BG_COLOR)
-
+#Control variables
+playerSpeed = 7
 #Player instances
-player = Player(10,80,20,(screen_heigh/2)-40,4,screen)
-opponent = Player(10,80,screen_width - 20,(screen_heigh/2)-40,4,screen)
+player = Player(10,80,20,(screen_heigh/2)-40,screen)
+opponent = Player(10,80,screen_width - 20,(screen_heigh/2)-40,screen)
 ball = Ball(20,20,(screen_width/2) - 10,(screen_heigh/2) - 10,7,screen)
 def main():
     #Execution boolean
@@ -30,6 +31,19 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 ExitGame = True
+
+
+        #Update            
+        keypressed = pygame.key.get_pressed()
+        if keypressed[pygame.K_w]:
+            player.move(-playerSpeed)
+        if keypressed[pygame.K_s]:
+            player.move(playerSpeed)
+
+        if keypressed[pygame.K_o]:
+            opponent.move(-playerSpeed)
+        if keypressed[pygame.K_l]:
+            opponent.move(playerSpeed)
 
 
         #Draw Elements
